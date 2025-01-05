0b9ddd47746cc11759a8dce6239ca0ba6d730a76
federicotempo <federicotempo98@gmail.com>
2025-01-05 16:15:55 -0300

    Update getWeather.js
    -Create getTemperature.
    -Create convertToCelsius.

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,17 +1,22 @@
-
-let city = "london";
-
 async function getWeather(city) {
   try {
     let response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=2VZAENS7BKUDBFCYA3CCTQXQC`
     );
-
     let data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.log("Error: ", error);
   }
 }
 
-getWeather(city);
+const getTemperature = (data) => {
+  const temperature = data.currentConditions.temp;
+  return temperature;
+};
+
+const convertToCelsius = (fahrenheit) => {
+  return ((fahrenheit - 32) * 5) / 9;
+};
+
+export { getWeather, getTemperature, convertToCelsius };
