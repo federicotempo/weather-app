631147dac7d6cb3401777028a966a1dfde543186
federicotempo <federicotempo98@gmail.com>
2025-01-05 17:18:40 -0300

    Create handleSearch and search
    -Creates handleSearchButton function to handle buttons.
    -Creates search function to search the city from user.

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -7,4 +7,4 @@ function getCityFromUser() {
   return city;
 }
 
-
+export { getCityFromUser };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import "./style.css"
+import "./styles.css";
+import { getWeather, getTemperature, convertToCelsius } from "./getWeather";
+import { getCityFromUser } from "./domManipulation";
+
+const handleSearchButton = () => {
+  const searchButton = document.querySelector(".search-button");
+  searchButton.addEventListener("click", search);
+};
+
+const search = async () => {
+  const city = getCityFromUser();
+  console.log(city);
+};
+
+handleSearchButton();
+
+// let city = getCity();
+
+// const data = await getWeather(city);
+// const temperature = getTemperature(data);
+// const temperatureInCelsius = convertToCelsius(temperature);
+// console.log(temperature);
+// console.log(temperatureInCelsius);
+/* 
+
+Usar API Visual Crossing
+Buscar una ubicación específica y alternar entre mostrar la temperatura en Celsius y Fahrenheit.
+Cambiar aspecto de la pagina según la temperatura o el tiempo.
+Usar async/await.
+
+
+*/
