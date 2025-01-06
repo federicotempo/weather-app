2c486942e7a8c3a3ab9ac7c08765c31743bbf410
federicotempo <federicotempo98@gmail.com>
2025-01-06 07:35:18 -0300

    Create handleSwitchButton
    -Creates switch button handler.
    -Creates switchToCelsius function.

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,3 +1,5 @@
+import { convertToCelsius } from "./getWeather";
+
 function getCityFromUser() {
   const city = document.querySelector("#city-input").value.toLowerCase();
   return city.split(" ").join("");
@@ -5,11 +7,32 @@ function getCityFromUser() {
 
 const renderData = (parameters) => {
   const [temperature, location, windSpeed, humidity, feelsLike] = parameters;
+
   document.querySelector("#temp").textContent = temperature;
   document.querySelector("#location").textContent = location;
   document.querySelector("#wind").textContent = windSpeed;
   document.querySelector("#humidity").textContent = humidity;
   document.querySelector("#feels-like").textContent = feelsLike;
+};
+
+const handleSwitchButton = () => {
+  const switchButton = document.querySelector(".toggle-unit");
+  switchButton.addEventListener("click", switchToCelsius);
+};
+
+const switchToCelsius = () => {
+  const temperature = document.querySelector("#temp").textContent;
+  const feelsLike = document.querySelector("#feels-like").textContent;
+
+  if (temperature !== "--" && feelsLike !== "--") {
+    const temperatureToCelsius = convertToCelsius(temperature);
+    const feelsLikeToCelsisus = convertToCelsius(feelsLike);
+
+    document.querySelector("#temp").textContent = `${temperatureToCelsius}°C`;
+    document.querySelector(
+      "#feels-like"
+    ).textContent = `${feelsLikeToCelsisus}°C`;
+  }
 };
 
 const focusInput = () => {
@@ -19,4 +42,4 @@ const focusInput = () => {
   });
 };
 
-export { getCityFromUser, focusInput, renderData };
+export { getCityFromUser, focusInput, renderData, handleSwitchButton };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import "./styles.css";
 import {
   getWeather,
   getTemperature,
-  convertToCelsius,
   getLocation,
   getFeelsLike,
   getHumidity,
   getWindSpeed,
 } from "./getWeather";
-import { getCityFromUser, focusInput, renderData } from "./domManipulation";
+import { getCityFromUser, focusInput, renderData, handleSwitchButton } from "./domManipulation";
 
 const handleSearchButton = () => {
   const searchButton = document.querySelector(".search-button");
@@ -36,23 +35,8 @@ const search = async () => {
 function initialize() {
   handleSearchButton();
   focusInput();
+  handleSwitchButton();
 }
 
 initialize();
 
-// let city = getCity();
-
-// const data = await getWeather(city);
-// const temperature = getTemperature(data);
-// const temperatureInCelsius = convertToCelsius(temperature);
-// console.log(temperature);
-// console.log(temperatureInCelsius);
-/* 
-
-Usar API Visual Crossing
-Buscar una ubicación específica y alternar entre mostrar la temperatura en Celsius y Fahrenheit.
-Cambiar aspecto de la pagina según la temperatura o el tiempo.
-Usar async/await.
-
-
-*/
